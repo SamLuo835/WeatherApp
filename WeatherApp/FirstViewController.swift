153a6b719d3edcc62dddd2e983bb0326370da455
Jianlin Luo <sam-law@live.cn>
2019-03-03 18:55:33 -0500

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Jianlin Luo on 2019-03-03.
+//  Copyright © 2019 Jianlin Luo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Jianlin Luo on 2019-03-03.
+//  Copyright © 2019 Jianlin Luo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
