b6e6b96b2c86f62db20ed666c8ecc4050db0371e
Vikki Wong <vikkiwong24@gmail.com>
2019-04-15 01:21:47 -0400

    Added background / button

--- WeatherApp/Class/CurrWeatherServiceUtility.swift
+++ WeatherApp/Class/CurrWeatherServiceUtility.swift
@@ -2,6 +2,9 @@
 //  CurrWeatherServiceUtility.swift
 //  WeatherApp
 //
+//   Create requests to obtain Current Weather
+//   information from OpenWeather API
+//
 //  Created by Vikki Wong on 2019-04-14.
 //  Copyright © 2019 Jianlin Luo. All rights reserved.
 //
@@ -9,17 +12,21 @@
 import UIKit
 
 class CurrWeatherServiceUtility: NSObject {
-    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
-    let converter = ConverterUtility.init()
     
+    // Main Delegate
+    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // Current Weather Request to get data from OpenWeather API
     func currentWeatherRequest(long:Double,lat:Double,handler: @escaping (NSDictionary)->()){
         var units = "metric"
         
+        // Change measurement unit according to Setting's boolean
         if(mainDelegate.celBoolean){
             units = "metric"
         }
         else {units = "imperial"}
         
+        // Initiate request to get JSON data for Current Weather
         let currentWeatherUrl = URL(string:"https://api.openweathermap.org/data/2.5/weather?units=\(units)&lat=\(lat)&lon=\(long)&appid=7d2f8fdc5ef5aba4d3197fc3bddd874e")!
         print(currentWeatherUrl)
         let task = URLSession.shared.dataTask(with: currentWeatherUrl){ (data, response, error) in
@@ -32,11 +39,13 @@ class CurrWeatherServiceUtility: NSObject {
                     do{
                         let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
                         
+                        // In case if retrieval of JSON is unsuccessful
                         guard let currentWeather = jsonResult as? NSMutableDictionary else {
                             print("invalid format")
                             return
                         }
                         
+                        // Append all neccessary data into an NSDictionary for later use
                         let main = currentWeather["main"] as? NSDictionary
                         let weather = currentWeather["weather"] as? NSArray
                         let name = currentWeather["name"] as? String
@@ -62,16 +71,17 @@ class CurrWeatherServiceUtility: NSObject {
                         let sys = currentWeather["sys"] as? NSDictionary
                         let country = sys!["country"] ?? "Unavailable"
                         
+                        // Format the sunrise and sunset date
                         let sunrise = sys!["sunrise"] as! Double 
                         let sunset = sys!["sunset"] as! Double
                         let sunriseDate = NSDate(timeIntervalSince1970: sunrise)
                         let sunsetDate = NSDate(timeIntervalSince1970: sunset)
-                        
                         let formatter =  DateFormatter()
                         formatter.dateFormat = "HH:mm:ss"
                         let formattedSunrise = formatter.string(from: sunriseDate as Date)
                         let formattedSunset = formatter.string(from: sunsetDate as Date)
                         
+                        // Put all data together into an NSDictioanry
                         let packagedWeather: NSDictionary = [
                             "name" : "\(name!), \(country)",
                             "main" : "\(mainWeather!)",
@@ -88,6 +98,7 @@ class CurrWeatherServiceUtility: NSObject {
                             "sunset" : "\(formattedSunset)"
                         ]
                         
+                        // Return the packaged weather data
                         handler(packagedWeather as NSDictionary)
                         
                     } catch {
